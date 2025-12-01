e39bc23ede0294c48397d93fe1ca02fd8491a4d1
faryid <m.akbar.alfaryid@gmail.com>
2025-12-01 15:39:28 +0700

    chore: set Next.js output & image optimization cfg

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // basePath: "/next",
+  // assetPrefix: "/next/",
+  // distDir: "dist",
+  // trailingSlash: true,
+  // trailingComma: "es5",
 };
 
 export default nextConfig;
